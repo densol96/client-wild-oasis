497347f9c874ac1cf2515befd98e600af825d642
Deniss Solovjovs <deniss11sol@gmail.com>
2024-09-24 18:47:33 +0300

    NEW: General configs, routes, layout set up

---
+++ app/account/page.js
@@ -0,0 +1,5 @@
+function Page() {
+  return <h1>Account page</h1>;
+}
+
+export default Page;
---
+++ app/cabins/page.js
@@ -0,0 +1,17 @@
+async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <>
+      <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Page;
