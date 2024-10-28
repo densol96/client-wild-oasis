1569c9fc0a6fdee2011283437fc51ec7a9805b3c
Deniss Solovjovs <deniss11sol@gmail.com>
2024-10-28 20:33:16 +0200

    NEW: Updating a Reservation implemented

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,7 +1,10 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import supabase from "./supabase";
+import { deleteBooking, getBooking, updateBooking } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function signInAction() {
   await signIn("google", {
@@ -17,7 +20,7 @@ export async function signOutAction() {
 
 export async function updateProfileAction(formData) {
   const session = await auth();
-  if (!session) throw new Error("Youmust be logged in!");
+  if (!session) throw new Error("You must be logged in!");
 
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
@@ -42,4 +45,35 @@ export async function updateProfileAction(formData) {
     console.error(error);
     throw new Error("Guest could not be updated");
   }
+
+  revalidatePath("/account/profile");
+}
+
+export async function deleteReservationAction(bookingId) {
+  const session = await auth();
+  if (!session)
+    throw new Error("Only authenticated users can delete their reservations!");
+  const booking = await getBooking(bookingId);
+  if (booking.guestId === session.user.id) {
+    await deleteBooking(bookingId);
+    revalidatePath("/account/reservations");
+  } else throw new Error("You are not allowed to delete this booking");
+}
+
+export async function updateBookingAction(formData) {
+  const bookingId = formData.get("bookingId");
+  const updatedFields = {
+    numGuests: +formData.get("numGuests"),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+  const session = await auth();
+  if (!session)
+    throw new Error("Only authenticated users can update their reservations!");
+  const booking = await getBooking(bookingId);
+  if (booking.guestId === session.user.id) {
+    await updateBooking(bookingId, updatedFields);
+    revalidatePath("/account/reservations");
+    revalidatePath(`/account/reservations/edit/${bookingId}`);
+    redirect("/account/reservations");
+  } else throw new Error("You are not allowed to update this booking");
 }
--- app/account/reservations/edit/[id]/page.js
+++ app/account/reservations/edit/[id]/page.js
@@ -1,15 +1,22 @@
-export default function Page() {
-  // CHANGE
-  const reservationId = 23;
-  const maxCapacity = 23;
+import { updateBookingAction } from "@/app/_lib/actions";
+import { getBooking, getCabin } from "@/app/_lib/data-service";
+import SubmitButton from "@/app/_components/SubmitButton";
+
+export default async function Page({ params }) {
+  const { numGuests, observations, cabinId } = await getBooking(params.id);
+  const { maxCapacity } = await getCabin(cabinId);
 
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Edit Reservation #{reservationId}
+        Edit Reservation #{params.id}
       </h2>
 
-      <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <form
+        className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+        action={updateBookingAction}
+      >
+        <input type="hidden" value={params.id} name="bookingId" />
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
           <select
@@ -17,6 +24,7 @@ export default function Page() {
             id="numGuests"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             required
+            defaultValue={numGuests}
           >
             <option value="" key="">
               Select number of guests...
@@ -34,15 +42,14 @@ export default function Page() {
             Anything we should know about your stay?
           </label>
           <textarea
+            defaultValue={observations}
             name="observations"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
           />
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update reservation
-          </button>
+          <SubmitButton>Update reservations</SubmitButton>
         </div>
       </form>
     </div>
