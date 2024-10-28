c8481f2106dfec2f8ad928cbf7ea9973d1deddc8
Deniss Solovjovs <deniss11sol@gmail.com>
2024-10-28 21:52:40 +0200

    NEW: Deleting a reservation with useOptimistic

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,32 @@
+"use client";
+
+import ReservationCard from "@/app/_components/ReservationCard";
+import { useOptimistic } from "react";
+import { deleteReservationAction } from "../_lib/actions";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) =>
+      curBookings.filter((booking) => booking.id !== bookingId)
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteReservationAction(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          onDelete={handleDelete}
+          booking={booking}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
